edd15ea363f010105d0a1dd342b0598dabfa4b28
neomdavid <neomanaogdavid@gmail.com>
2024-12-01 11:21:11 +0800

    First Commit

---
+++ 12-http.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to our home page");
+    return;
+  }
+  if (req.url === "/about") {
+    res.end("here is our short history");
+    return;
+  }
+  res.end(`
+    <h1>Oops!</1=h1>
+    <p> We can't seem to find the page you are looking for</p>
+    <a href='/'>back home</a>`);
+});
+
+server.listen(5000);
---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+const {john, peter} = require('./4-names');
+const data = require('./6-alternative-flavor');
+const sayHi = require('./5-utils');
+console.log(data)
+require('./7-mind-grenade')
+sayHi(john);
+
+// sayHi('susan');
+// sayHi(john);
